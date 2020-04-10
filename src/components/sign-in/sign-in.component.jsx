f8fb9d8e98d5f3fb457f22fdd4c1ab5b64ac06e6
tom6yang <59739586+tom6yang@users.noreply.github.com>
2020-04-10 09:45:00 -0400

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -17,12 +17,12 @@ class SignIn extends React.Component{
         }
     }
 
-    handbleSubmit = event => {
+    handleSubmit = event => {
         event.preventDefault();
         this.setState({email: '', password: ''})
     }
 
-    handbleChange = event => {
+    handleChange = event => {
         const {value, name} = event.target;
         this.setState({[name]: value})
     }
@@ -33,12 +33,12 @@ class SignIn extends React.Component{
                <h2>I already have an account</h2>
                <span>Sign in with your email and password</span>
 
-               <form onSubmit={this.handbleSubmit}>
+               <form onSubmit={this.handleSubmit}>
                    <FormInput 
                         name="email" 
                         type="email" 
                         value={this.state.email} 
-                        handleChange = {this.handbleChange}
+                        handleChange = {this.handleChange}
                         label="email"
                         required
                    />
@@ -46,15 +46,14 @@ class SignIn extends React.Component{
                         name="password" 
                         type="password" 
                         value={this.state.password} 
-                        handleChange = {this.handbleChange}
+                        handleChange = {this.handleChange}
                         label="password"
                         required
                    />
                    <div className='buttons'>
                         <CustomButton type="submit">SIGN IN</CustomButton>
                         <CustomButton onClick={ signInWithGoogle } isGoogleSignIn>
-                            {' '}
-                            Sign in with Google {' '}
+                            Sign in with Google 
                         </CustomButton>
                    </div>
                </form>
